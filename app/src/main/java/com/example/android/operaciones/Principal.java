ffae2dc6c4df03011e88db9eb60c3dc6c0c7f84f
kortiz1 <playstorekvin@gmail.com>
2017-08-15 21:20:04 -0500

    Interfaz grafica y bilingue

--- app/src/main/java/com/example/android/operaciones/Principal.java
+++ app/src/main/java/com/example/android/operaciones/Principal.java
@@ -3,28 +3,54 @@ package com.example.android.operaciones;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Principal extends AppCompatActivity {
 
-    private TxtView res;
+    private TextView res;
     private EditText n1, n2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-         res = (TextView)findViewById(R.id.lblResultado);
-         n1 = (TextView)findViewById(R.id.txtNumeroUno);
-         n2 = (TextView)findViewById(R.id.txtNumeroDos);
+        res = (TextView) findViewById(R.id.lblResultado);
+        n1 = (EditText) findViewById(R.id.txtNumeroUno);
+        n2 = (EditText) findViewById(R.id.txtNumeroDos);
 
     }
 
-    public void calcular(View v){
-        int num1, num2, res;
+    public void calcular(View v) {
+        int num1, num2, resultado;
         num1 = Integer.parseInt(n1.getText().toString());
         num2 = Integer.parseInt(n2.getText().toString());
-        res = num1+num2;
+        resultado = num1 + num2;
+        res.setText("" + resultado);
 
-        res.setText(""+resultado);
+    }
+
+    public void borrar(View v) {
+        res.setText("");
+        n1.setText("");
+        n2.setText("");
+        n1.requestFocus();
+    }
+
+    public boolean validar() {
+        if (n1.getText().toString().isEmpty()) {
+            n1.setError("DIGITE EL NÚMERO 1");
+            return false;
+
+        }
+
+
+        if (n2.getText().toString().isEmpty()) {
+            n2.setError("DIGITE EL NÚMERO 2");
+            return false;
+
+        }
+        return true;
     }
 }
+
